03cee867546c08307726dd1a0d39cc89f86eb2b6
bastien.hoffstetter@etu.u-bordeaux.fr <bastien.hoffstetter@etu.u-bordeaux.fr>
2015-05-20 18:02:14 +0200

    Début configuration interface

---
+++ ProjetBD/Fournisseur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetBD
+{
+    class Fournisseur
+    {
+        public List<Musicien> GetMusiciens()
+        {
+            ClassiqueEntities context = new ClassiqueEntities();
+            var mus = (from m in context.Musicien
+                       orderby m.Nom_Musicien
+                       select m);
+
+            return mus.ToList();
+        }
+    }
+}
