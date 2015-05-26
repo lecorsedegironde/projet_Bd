c9836bbee2791b5d9433c58661a78bf5a031a364
bastien.hoffstetter@etu.u-bordeaux.fr <bastien.hoffstetter@etu.u-bordeaux.fr>
2015-05-26 15:19:44 +0200

    Récupération Infos avant alignement

---
+++ ProjetBD/ImageConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ProjetBD
+{
+    class ImageConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            if (value != null && value is byte[])
+            {
+                MemoryStream stream = new MemoryStream((byte []) value);
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = stream;
+                image.EndInit();
+                return image;
+            }
+            if (value == null)
+            {
+                BitmapImage image = new BitmapImage();
+                // On affiche, pas d'image
+                return image;
+            }
+            return null;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            throw new Exception("Pas encore implémenté");
+        }
+    }
+}
